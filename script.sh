dd866cf7237619f739e73a0853c3d963175ca9ba
Archibald Lu <chienius@outlook.com>
2014-01-06 18:01:41 +0800

    Cookie时限撤销优化; 移动脚本配置关联文件至 ~/.config/tjyfz/ 当中

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 welcomeMsg="\033[1mTJYFZ console toys\033[0m\n\033[33m我们不用很麻烦很累就可以上平台\n     \033[0mPresented by Chienius\n     (c) 2013 chienius.com \n"
+configDir="~/.config/tjyfz"
+
+if [ ! -d "$configDir"  ]; then
+	echo -e "You are running the script for the first time. \nPlease use 'login' conmmand to load initial settings first.\n"
+	mkdir -p $configDir > /dev/null	#set a config directory
+fi
 
 if [ $# -lt 1 ]; then
 	echo  -e $welcomeMsg
@@ -18,13 +24,13 @@ if [ $1 = "login" ]; then
 	read -p "Enter Password: " pass
 	stty echo
 
-	loginrev=`curl http://www.tjyfz1.edu.sh.cn/chsr1.asp -d "userid=$user&pwd=$pass&leibie=xuesheng" -c cookie.txt --stderr /dev/null | iconv -f gbk -t utf-8 | grep "用户或密码错误"`
+	loginrev=`curl http://www.tjyfz1.edu.sh.cn/chsr1.asp -d "userid=$user&pwd=$pass&leibie=xuesheng" -c $configDir/cookie.txt --stderr /dev/null | iconv -f gbk -t utf-8 | grep "用户或密码错误"`
 
 	if [ -n "$loginrev" ]; then 
 		echo -e "\nIncorrect password! Try again!"
 	else
 		echo -e "\nLogin succeeded!"
-		sed -i 's/1386345600/0/' cookie.txt
+		sed -i 's/13.*cok/0\tcok/' $configDir/cookie.txt
 	fi
 
 elif [ $1 = "homework" ]; then
@@ -35,7 +41,7 @@ elif [ $1 = "homework" ]; then
 	fi
 	date -d $date +%B%d日，%A
 	echo 
-	hwContent=`curl http://www.tjyfz1.edu.sh.cn/cjcx/jiaoshi/plshuru/zybz/zybz.asp?rq=$date -b cookie.txt --stderr /dev/null | iconv -f gbk -t utf-8 | tr -d "\r\n"`
+	hwContent=`curl http://www.tjyfz1.edu.sh.cn/cjcx/jiaoshi/plshuru/zybz/zybz.asp?rq=$date -b $configDir/cookie.txt --stderr /dev/null | iconv -f gbk -t utf-8 | tr -d "\r\n"`
 	hwContent=${hwContent#*body>}		#delete useless prefix
 	hwContent=${hwContent%<script*}		#delete useless suffix
 	hwContent=${hwContent##*listtable\">}
